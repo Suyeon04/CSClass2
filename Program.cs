416d04d76f90f9b9d0b8a38c0e46934645ef087a
Suyeon04 <s2026@e-mirim.hs.kr>
2022-09-29 20:19:06 +0900

    #4 구조체

--- Program.cs
+++ Program.cs
@@ -45,6 +45,69 @@ namespace CSClass231
             rx = x + vx;
             ry = y + vy;
         }
+
+
+
+            Point point;
+            point.x = 10;
+            point.y = 10;
+            point = new Point(3, 5);
+            Console.WriteLine(point.x + " / " + point.y);
+
+            PointClass pointClassA = new PointClass(10, 20);
+            PointClass pointClassB = pointClassA;
+            pointClassB.x = 100; pointClassB.y = 200;
+            Console.WriteLine(pointClassA.x + "/" + pointClassA.y);
+            Console.WriteLine(pointClassB.x + "/" + pointClassB.y);
+
+            PointStruct pointStructA = new PointStruct(10, 20);
+            PointStruct pointStructB = pointStructA;
+            pointStructB.x = 100; pointStructB.y = 200;
+            Console.WriteLine(pointStructA.x + "/" + pointStructA.y);
+            Console.WriteLine(pointStructB.x + "/" + pointStructB.y);
+
+
+            struct Point
+        {
+            public int x;
+            public int y;
+            public string testA;
+            public string testB;
+            public Point(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+                this.testA = "초기화";
+                this.testB = "초기화";
+            }
+            public Point(int x, int y, string test)
+            {
+                this.x = x;
+                this.y = y;
+                this.testA = test;
+                this.testB = test;
+            }            
+        }
+        class PointClass
+        {
+            public int x;
+            public int y;
+            public PointClass(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+        struct PointStruct
+        {
+            public int x;
+            public int y;
+            public PointStruct(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
         }
     }
 }
